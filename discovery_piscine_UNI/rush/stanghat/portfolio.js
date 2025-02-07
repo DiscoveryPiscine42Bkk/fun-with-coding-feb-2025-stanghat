2e2d1f82935130860890b3dba6e77ee2c2c71172
stanghat <66050406@kmitl.ac.th>
2025-02-07 20:43:51 +0700

    Update portfolio.js

--- discovery_piscine_UNI/rush/stanghat/portfolio.js
+++ discovery_piscine_UNI/rush/stanghat/portfolio.js
@@ -1,1 +1,11 @@
-
+    <script>
+        $(document).ready(function () {
+            $('.navigation a').on('click', function (event) {
+                event.preventDefault();
+                let target = $(this).attr('href');
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top - 50
+                }, 800);
+            });
+        });
+    </script>
